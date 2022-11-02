ed138c3bcd465e26839d6394b1be9fdec50c71ce
Антон Кондрашов <greg_oskol@inbox.ru>
2022-11-02 18:01:51 +0300

    Kondrash0211

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -10,5 +10,7 @@ class IsAdminOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return (
-            request.method in permissions.SAFE_METHODS or request.user.is_admin
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
+            and request.user.role == "admin"
         )
